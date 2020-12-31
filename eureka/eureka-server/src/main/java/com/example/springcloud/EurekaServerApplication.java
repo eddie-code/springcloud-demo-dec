e440b6d73ec99d42271eb2ae674a7611b271c0ad
eddie-code <231071372@qq.com>
2020-12-31 10:57:00 +0800

    3-6 搭建注册中心

---
+++ eureka/eureka-server/src/main/java/com/example/springcloud/EurekaServerApplication.java
@@ -0,0 +1,27 @@
+package com.example.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author eddie.lee
+ * @ProjectName springcloud-demo-dec
+ * @Package com.example.springcloud
+ * @ClassName EurekaServerApplication
+ * @blog blog.eddilee.cn
+ * @description
+ * @date created in 2020-12-31 10:35
+ * @modified by
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
